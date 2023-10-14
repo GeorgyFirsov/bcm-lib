bc7adb2b3c8cb2e9e2c883946f8aa572715af0cc
GeorgyFirsov <gfirsov007@gmail.com>
2023-10-14 17:51:30 +0000

    fix: comment
    Signed-off-by: GeorgyFirsov <gfirsov007@gmail.com>

--- include/modes/cmac/cmac.h
+++ include/modes/cmac/cmac.h
@@ -35,7 +35,7 @@ typedef enum tag_cmac_verify_result
 
 
 /**
- * @brief Computes CMAC using 182-bit block cipher.
+ * @brief Computes CMAC using 128-bit block cipher.
  *        Tag is 64 bit long and is stored in most significant
  *        bits of 128-bit output value.
  * 
